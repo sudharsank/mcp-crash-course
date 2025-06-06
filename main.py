bfb57f7b7160903ed9c7c1179278593f535b878d
Sudharsan K <sudharsan_1985@live.in>
2025-06-06 12:11:38 +0800

    Enhance main.py and update dependencies for improved functionality
    - Added new imports and initialized ChatOpenAI and StdioServerParameters in main.py.
    - Updated pyproject.toml to include isort as a dependency.
    - Modified uv.lock to reflect the addition of isort version 6.0.1.
    - Added a blank line in weather_server.py for better code readability.

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import asyncio
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from langgraph.prebuilt import create_react_agent
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.stdio import stdio_client
+
 load_dotenv()
-print(os.getenv("OPENAI_API_KEY")) # This is a test to see if the API key is loaded
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/sudharsank/Documents/Workouts/MCP/mcp-crash-course/servers/weather_server.py"]
+)
 
 async def main():
     print("Hello from mcp-crash-course!")
